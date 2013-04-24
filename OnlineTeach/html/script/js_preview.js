3fe31be66ec323f28f9acc5030a9b1147c73e805
Lov Why <rankun203@gmail.com>
2013-04-24 14:36:36 +0800

    为createWork.html添加一个方法:addOneAnswer()，可以通过这个方法添加一个题目。

---
+++ OnlineTeach/html/script/js_preview.js
@@ -0,0 +1,49 @@
+ // JavaScript Document
+
+function addDynamicAnswer(){
+	
+}
+function addOneAnswer(){
+	var dsiLength = document.getElementsByClassName("dynamicAnswerItem").length;
+	var randSub = dsiLength - 1 - (Number)(((Math.random() * 10 / 3) + "").substring(0, 1));
+	var insertedNode = document.getElementsByClassName("dynamicAnswerItem").item(randSub).cloneNode(10);
+	document.getElementById("daTit").parentNode.insertBefore(insertedNode, document.getElementById("daTit").nextSibling);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
