d0ed0957fa037452c9660d365e9b13d798ec69ae
Kun Ran <rankun203@gmail.com>
2013-10-15 08:49:49 +0800

    修改PPT，加入指导老师……

--- src/main/java/com/teachMng/onlineTeach/action/WorkAction.java
+++ src/main/java/com/teachMng/onlineTeach/action/WorkAction.java
@@ -36,277 +36,278 @@ import com.teachMng.onlineTeach.service.IExerciseSetService;
 @Component("workAction")
 @Scope("singleton")
 public class WorkAction extends ActionSupport implements ServletResponseAware {
-	private static final long serialVersionUID = 1L;
-	private String esId;
-	private String topicId;
-	private double score;
-	private String teacComment;
-	private String type;
-	private String answer;
-	private IExerciseSetService ess;
-	private HttpServletResponse response;
-	private IExerciseSetSelectionExerciseService esseService;
-	private IExerciseSetQuestionExerciseService esqeService;
-	private IExerciseSetCompletionExerciseService esceService;
-	private IExerciseSetJudgeExerciseService esjeService;
+    private static final long serialVersionUID = 1L;
+    private String esId;
+    private String topicId;
+    private double score;
+    private String teacComment;
+    private String type;
+    private String answer;
+    private IExerciseSetService ess;
+    private HttpServletResponse response;
+    private IExerciseSetSelectionExerciseService esseService;
+    private IExerciseSetQuestionExerciseService esqeService;
+    private IExerciseSetCompletionExerciseService esceService;
+    private IExerciseSetJudgeExerciseService esjeService;
 
-	private IAnswerHistoryService ahs;
-	private Queue<AnswerHistory> ah = new LinkedList<AnswerHistory>();
+    private IAnswerHistoryService ahs;
+    private Queue<AnswerHistory> ah = new LinkedList<AnswerHistory>();
 
-	public String workReply() {
-		ess.workReply(Integer.parseInt(esId), type, Integer.parseInt(topicId),
-				answer);
-		AnswerHistory answerHistory = new AnswerHistory();
-		answerHistory.setEsId(Integer.parseInt(esId));
-		answerHistory.settId(Integer.parseInt(topicId));
-		answerHistory.setType(type);
-		ah.add(answerHistory);
-		ahs.insert(answerHistory);
-        return null;
-	}
-
-	/**
-	 * 题目打分
-	 * http://localhost:8080/OnlineTeach/work/workMark?type=completion&esId=1&topicId=4&score=90.5&teacComment=hdslafjkldsaf
-	 */
-	public String workMark() {
-		if ("selection".equals(type)) {
-			ExerciseSetSelectionExercise esse = esseService.findByEsIdSeId(
-					Integer.parseInt(esId), Integer.parseInt(topicId));
-			esse.setStuScore(score);
-			esse.setTeacherComment(teacComment);
-			esseService.update(esse);
-		} else if ("completion".equals(type)) {
-			ExerciseSetCompletionExercise esce = esceService.findByEsIdCeId(
-					Integer.parseInt(esId), Integer.parseInt(topicId));
-			esce.setStuScore(score);
-			esce.setTeacherComment(teacComment);
-			esceService.update(esce);
-		} else if ("question".equals(type)) {
-			ExerciseSetQuestionExercise esqe = esqeService.findByEsIdQeId(
-					Integer.parseInt(esId), Integer.parseInt(topicId));
-			esqe.setStuScore(score);
-			esqe.setTeacherComment(teacComment);
-			esqeService.update(esqe);
-		} else if ("judge".equals(type)) {
-			ExerciseSetJudgeExercise esje = esjeService.findByEsIdJeId(
-					Integer.parseInt(esId), Integer.parseInt(topicId));
-			esje.setStuScore(score);
-			esje.setTeacherComment(teacComment);
-			esjeService.update(esje);
-		}
-        return null;
-	}
-
-	/**
-	 * 获取已经回答的题 访问URL:http://localhost:8080/OnlineTeach/work/getRepliedWork [{
-	 * type:题型, topic:标题, stuAnswer:学生回答, ansDate:回答日期, stuName:回答学生姓名,
-	 * stdAnswer:正确答案, topicId:题目编号, esId:试卷编号 }]
-	 */
-	public String getRepliedWork() {
-		if (ah.isEmpty()) {
-			return null;
-		}
-		String json = "[";
-		AnswerHistory ans = null;
-		SelectionExercise _se = null;
-		CompletionExercise _ce = null;
-		JudgeExercise _je = null;
-		QuestionExercise _qe = null;
-		Student stu = null;
-		boolean flag = false;
-		while (!ah.isEmpty()) {
-			ans = ah.poll();
-			if (flag)
-				json += ",{";
-			else
-				json += "{";
-			if ("selection".equals(ans.getType())) {
-				ExerciseSetSelectionExercise _esse = null;
-				_esse = esseService.findByEsIdSeId(ans.getEsId(), ans.gettId());
-				_se = _esse.getSe();
-				stu = _esse.getEs().getStudent();
-				json += getJson("selection", _se.getFullTopic(),
-						_esse.getStuAnswer(), ans.getDate(), stu.getStuName(),
-						_se.getStdAnswer() + "", _se.getStdScore(), ans.gettId(), ans.getEsId());
-			} else if ("completion".equals(ans.getType())) {
-				ExerciseSetCompletionExercise _esce = null;
-				_esce = esceService.findByEsIdCeId(ans.getEsId(), ans.gettId());
-				_ce = _esce.getCe();
-				stu = _esce.getEs().getStudent();
-				json += getJson("completion", replaceCompletion(_ce.getFullTopic()),
-						_esce.getStuAnswer(), ans.getDate(), stu.getStuName(),
-						_ce.getStdAnswer(), _ce.getStdScore(), ans.gettId(), ans.getEsId());
-			} else if ("question".equals(ans.getType())) {
-				ExerciseSetQuestionExercise _esqe = null;
-				_esqe = esqeService.findByEsIdQeId(ans.getEsId(), ans.gettId());
-				_qe = _esqe.getQe();
-				stu = _esqe.getEs().getStudent();
-				json += getJson("question", _qe.getFullTopic(),
-						_esqe.getStuAnswer(), ans.getDate(), stu.getStuName(),
-						_qe.getStdKeyword(), _qe.getStdScore(), ans.gettId(), ans.getEsId());
-			} else if ("judge".equals(ans.getType())) {
-				ExerciseSetJudgeExercise _esje = null;
-				_esje = esjeService.findByEsIdJeId(ans.getEsId(), ans.gettId());
-				_je = _esje.getJe();
-				stu = _esje.getEs().getStudent();
-				json += getJson("judge", _je.getFullTopic(),
-						_esje.isStuAnswerIsRight() + "", ans.getDate(),
-						stu.getStuName(), _je.isStdAnswerIsRight() + "", _je.getStdScore(),
-						ans.gettId(), ans.getEsId());
-			}
-			json += "}";
-			flag = true;
-		}
-		json += "]";
-		out().print(json);
-		System.out.println(json);
+    public String workReply() {
+        ess.workReply(Integer.parseInt(esId), type, Integer.parseInt(topicId), answer);
+        AnswerHistory answerHistory = new AnswerHistory();
+        answerHistory.setEsId(Integer.parseInt(esId));
+        answerHistory.settId(Integer.parseInt(topicId));
+        answerHistory.setType(type);
+        ah.add(answerHistory);
+        ahs.insert(answerHistory);
         return null;
     }
 
-	private String getJson(String type, String topic, String stuAnswer,
-			Date date, String stuName, String stdAnswer, double stdScore, int topicId, int esId) {
-		String json = "";
-		DateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss E");
-		json += "\"type\":\"" + type;
-		json += "\",\"topic\":\"" + topic;
-		json += "\",\"stuAnswer\":\"" + stuAnswer;
-		json += "\",\"ansDate\":\"" + df.format(date);
-		json += "\",\"stuName\":\"" + stuName;
-		json += "\",\"stdAnswer\":\"" + stdAnswer.replace("\"", "\\\"");
-		json += "\",\"stdScore\":\"" + stdScore;
-		json += "\",\"topicId\":\"" + topicId;
-		json += "\",\"esId\":\"" + esId + "\"";
-		return json;
-	}
-	private String replaceCompletion(String topic) {
-		String tmp = topic;
-		if(-1 == tmp.indexOf("@space@")) return topic;
-		String s1, s2, s = "";
-		int index = -1, i = 0;
-		while(-1 != (index = tmp.indexOf("@space@"))) {
-			s1 = tmp.substring(0, index + 7);
-			s2 = tmp.substring(index + 7, tmp.length());
-			s += s1.replace("@space@", "@" + (i++) + "@");
-			tmp = s2;
-		}
-		return s;
-	}
-	public double getScore() {
-		return score;
-	}
+    /**
+     * 题目打分
+     * http://localhost:8080/OnlineTeach/work/workMark?type=completion&esId=1&topicId=4&score=90.5&teacComment=hdslafjkldsaf
+     */
+    public String workMark() {
+        if ("selection".equals(type)) {
+            ExerciseSetSelectionExercise esse = esseService.findByEsIdSeId(
+                    Integer.parseInt(esId), Integer.parseInt(topicId));
+            esse.setStuScore(score);
+            esse.setTeacherComment(teacComment);
+            esseService.update(esse);
+        } else if ("completion".equals(type)) {
+            ExerciseSetCompletionExercise esce = esceService.findByEsIdCeId(
+                    Integer.parseInt(esId), Integer.parseInt(topicId));
+            esce.setStuScore(score);
+            esce.setTeacherComment(teacComment);
+            esceService.update(esce);
+        } else if ("question".equals(type)) {
+            ExerciseSetQuestionExercise esqe = esqeService.findByEsIdQeId(
+                    Integer.parseInt(esId), Integer.parseInt(topicId));
+            esqe.setStuScore(score);
+            esqe.setTeacherComment(teacComment);
+            esqeService.update(esqe);
+        } else if ("judge".equals(type)) {
+            ExerciseSetJudgeExercise esje = esjeService.findByEsIdJeId(
+                    Integer.parseInt(esId), Integer.parseInt(topicId));
+            esje.setStuScore(score);
+            esje.setTeacherComment(teacComment);
+            esjeService.update(esje);
+        }
+        return null;
+    }
 
-	public void setScore(double score) {
-		this.score = score;
-	}
+    /**
+     * 获取已经回答的题 访问URL:http://localhost:8080/OnlineTeach/work/getRepliedWork [{
+     * type:题型, topic:标题, stuAnswer:学生回答, ansDate:回答日期, stuName:回答学生姓名,
+     * stdAnswer:正确答案, topicId:题目编号, esId:试卷编号 }]
+     */
+    public String getRepliedWork() {
+        if (ah.isEmpty()) {
+            return null;
+        }
+        String json = "[";
+        AnswerHistory ans = null;
+        SelectionExercise _se = null;
+        CompletionExercise _ce = null;
+        JudgeExercise _je = null;
+        QuestionExercise _qe = null;
+        Student stu = null;
+        boolean flag = false;
+        while (!ah.isEmpty()) {
+            ans = ah.poll();
+            if (flag)
+                json += ",{";
+            else
+                json += "{";
+            if ("selection".equals(ans.getType())) {
+                ExerciseSetSelectionExercise _esse = null;
+                _esse = esseService.findByEsIdSeId(ans.getEsId(), ans.gettId());
+                _se = _esse.getSe();
+                stu = _esse.getEs().getStudent();
+                json += getJson("selection", _se.getFullTopic(),
+                        _esse.getStuAnswer(), ans.getDate(), stu.getStuName(),
+                        _se.getStdAnswer() + "", _se.getStdScore(), ans.gettId(), ans.getEsId());
+            } else if ("completion".equals(ans.getType())) {
+                ExerciseSetCompletionExercise _esce = null;
+                _esce = esceService.findByEsIdCeId(ans.getEsId(), ans.gettId());
+                _ce = _esce.getCe();
+                stu = _esce.getEs().getStudent();
+                json += getJson("completion", replaceCompletion(_ce.getFullTopic()),
+                        _esce.getStuAnswer(), ans.getDate(), stu.getStuName(),
+                        _ce.getStdAnswer(), _ce.getStdScore(), ans.gettId(), ans.getEsId());
+            } else if ("question".equals(ans.getType())) {
+                ExerciseSetQuestionExercise _esqe = null;
+                _esqe = esqeService.findByEsIdQeId(ans.getEsId(), ans.gettId());
+                _qe = _esqe.getQe();
+                stu = _esqe.getEs().getStudent();
+                json += getJson("question", _qe.getFullTopic(),
+                        _esqe.getStuAnswer(), ans.getDate(), stu.getStuName(),
+                        _qe.getStdKeyword(), _qe.getStdScore(), ans.gettId(), ans.getEsId());
+            } else if ("judge".equals(ans.getType())) {
+                ExerciseSetJudgeExercise _esje = null;
+                _esje = esjeService.findByEsIdJeId(ans.getEsId(), ans.gettId());
+                _je = _esje.getJe();
+                stu = _esje.getEs().getStudent();
+                json += getJson("judge", _je.getFullTopic(),
+                        _esje.isStuAnswerIsRight() + "", ans.getDate(),
+                        stu.getStuName(), _je.isStdAnswerIsRight() + "", _je.getStdScore(),
+                        ans.gettId(), ans.getEsId());
+            }
+            json += "}";
+            flag = true;
+        }
+        json += "]";
+        out().print(json);
+        System.out.println(json);
+        return null;
+    }
 
-	public String getTeacComment() {
-		return teacComment;
-	}
+    private String getJson(String type, String topic, String stuAnswer,
+                           Date date, String stuName, String stdAnswer, double stdScore, int topicId, int esId) {
+        String json = "";
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss E");
+        json += "\"type\":\"" + type;
+        json += "\",\"topic\":\"" + topic;
+        json += "\",\"stuAnswer\":\"" + stuAnswer;
+        json += "\",\"ansDate\":\"" + df.format(date);
+        json += "\",\"stuName\":\"" + stuName;
+        json += "\",\"stdAnswer\":\"" + stdAnswer.replace("\"", "\\\"");
+        json += "\",\"stdScore\":\"" + stdScore;
+        json += "\",\"topicId\":\"" + topicId;
+        json += "\",\"esId\":\"" + esId + "\"";
+        return json;
+    }
 
-	public void setTeacComment(String teacComment) {
-		this.teacComment = teacComment;
-	}
+    private String replaceCompletion(String topic) {
+        String tmp = topic;
+        if (-1 == tmp.indexOf("@space@")) return topic;
+        String s1, s2, s = "";
+        int index = -1, i = 0;
+        while (-1 != (index = tmp.indexOf("@space@"))) {
+            s1 = tmp.substring(0, index + 7);
+            s2 = tmp.substring(index + 7, tmp.length());
+            s += s1.replace("@space@", "@" + (i++) + "@");
+            tmp = s2;
+        }
+        return s;
+    }
 
-	public IExerciseSetSelectionExerciseService getEsseService() {
-		return esseService;
-	}
+    public double getScore() {
+        return score;
+    }
 
-	@Resource(name = "esseService")
-	public void setEsseService(IExerciseSetSelectionExerciseService esseService) {
-		this.esseService = esseService;
-	}
+    public void setScore(double score) {
+        this.score = score;
+    }
 
-	public IExerciseSetQuestionExerciseService getEsqeService() {
-		return esqeService;
-	}
+    public String getTeacComment() {
+        return teacComment;
+    }
 
-	@Resource(name = "esqeService")
-	public void setEsqeService(IExerciseSetQuestionExerciseService esqeService) {
-		this.esqeService = esqeService;
-	}
+    public void setTeacComment(String teacComment) {
+        this.teacComment = teacComment;
+    }
 
-	public IExerciseSetCompletionExerciseService getEsceService() {
-		return esceService;
-	}
+    public IExerciseSetSelectionExerciseService getEsseService() {
+        return esseService;
+    }
 
-	@Resource(name = "esceService")
-	public void setEsceService(IExerciseSetCompletionExerciseService esceService) {
-		this.esceService = esceService;
-	}
+    @Resource(name = "esseService")
+    public void setEsseService(IExerciseSetSelectionExerciseService esseService) {
+        this.esseService = esseService;
+    }
 
-	public IExerciseSetJudgeExerciseService getEsjeService() {
-		return esjeService;
-	}
+    public IExerciseSetQuestionExerciseService getEsqeService() {
+        return esqeService;
+    }
 
-	@Resource(name = "esjeService")
-	public void setEsjeService(IExerciseSetJudgeExerciseService esjeService) {
-		this.esjeService = esjeService;
-	}
+    @Resource(name = "esqeService")
+    public void setEsqeService(IExerciseSetQuestionExerciseService esqeService) {
+        this.esqeService = esqeService;
+    }
 
-	public IAnswerHistoryService getAhs() {
-		return ahs;
-	}
+    public IExerciseSetCompletionExerciseService getEsceService() {
+        return esceService;
+    }
 
-	@Resource(name = "answerHistoryService")
-	public void setAhs(IAnswerHistoryService ahs) {
-		this.ahs = ahs;
-	}
+    @Resource(name = "esceService")
+    public void setEsceService(IExerciseSetCompletionExerciseService esceService) {
+        this.esceService = esceService;
+    }
 
-	public String getEsId() {
-		return esId;
-	}
+    public IExerciseSetJudgeExerciseService getEsjeService() {
+        return esjeService;
+    }
 
-	public void setEsId(String esId) {
-		this.esId = esId;
-	}
+    @Resource(name = "esjeService")
+    public void setEsjeService(IExerciseSetJudgeExerciseService esjeService) {
+        this.esjeService = esjeService;
+    }
 
-	public String getTopicId() {
-		return topicId;
-	}
+    public IAnswerHistoryService getAhs() {
+        return ahs;
+    }
 
-	public void setTopicId(String topicId) {
-		this.topicId = topicId;
-	}
+    @Resource(name = "answerHistoryService")
+    public void setAhs(IAnswerHistoryService ahs) {
+        this.ahs = ahs;
+    }
 
-	public String getType() {
-		return type;
-	}
+    public String getEsId() {
+        return esId;
+    }
 
-	public void setType(String type) {
-		this.type = type;
-	}
+    public void setEsId(String esId) {
+        this.esId = esId;
+    }
 
-	public String getAnswer() {
-		return answer;
-	}
+    public String getTopicId() {
+        return topicId;
+    }
 
-	public void setAnswer(String answer) {
-		this.answer = answer;
-	}
+    public void setTopicId(String topicId) {
+        this.topicId = topicId;
+    }
 
-	public IExerciseSetService getEss() {
-		return ess;
-	}
+    public String getType() {
+        return type;
+    }
 
-	@Resource(name = "exerciseSetService")
-	public void setEss(IExerciseSetService ess) {
-		this.ess = ess;
-	}
+    public void setType(String type) {
+        this.type = type;
+    }
 
-	public PrintWriter out() {
-		try {
-			response.setCharacterEncoding("utf-8");
-			return response.getWriter();
-		} catch (IOException e) {
-			System.err.println("系统错误，获取response对象失败！");
-			e.printStackTrace();
-		}
-		return null;
-	}
+    public String getAnswer() {
+        return answer;
+    }
 
-	@Override
-	public void setServletResponse(HttpServletResponse response) {
-		this.response = response;
-	}
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public IExerciseSetService getEss() {
+        return ess;
+    }
+
+    @Resource(name = "exerciseSetService")
+    public void setEss(IExerciseSetService ess) {
+        this.ess = ess;
+    }
+
+    public PrintWriter out() {
+        try {
+            response.setCharacterEncoding("utf-8");
+            return response.getWriter();
+        } catch (IOException e) {
+            System.err.println("系统错误，获取response对象失败！");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public void setServletResponse(HttpServletResponse response) {
+        this.response = response;
+    }
 }
